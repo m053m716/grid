59e5b7efc7a2dde051399c49926d80ee535a61f6
Max-MI115 <max.murphy11@gmail.com>
2022-03-14 13:17:42 -0400

    Add export to 4D grid

--- array_to_grid.m
+++ array_to_grid.m
@@ -23,14 +23,22 @@ function [G, idx] = array_to_grid(V)
 if size(V, 1) ~= 64
     error('This only works for 8x8 grids (64 elements!)');
 end
-
 idx = flipud(reshape(1:64, 8, 8));
-% Reshape so we have an indexing grid of identical elements (through time),
-% and then add a "step" array to the third dimension (so, time), which
-% steps the sample increment by 64 elements for each time-sample such that
-% the indexing works correctly:
-idx = repmat(idx, 1, 1, size(V, 2)) + ... % This is the "address" of each row from the sample matrix (nChannels x nSamples)
-    reshape(0:64:((size(V,2)-1)*64), 1, 1, size(V, 2)); % This is the spot in our (nRow x nColumn x nSamples) grid array (tensor) where each sample should go.
-G = V(idx);
+        % Reshape so we have an indexing grid of identical elements (through time),
+        % and then add a "step" array to the third dimension (so, time), which
+        % steps the sample increment by 64 elements for each time-sample such that
+        % the indexing works correctly:
+        idx = repmat(idx, 1, 1, size(V, 2)) + ... % This is the "address" of each row from the sample matrix (nChannels x nSamples)
+            reshape(0:64:((size(V,2)-1)*64), 1, 1, size(V, 2)); % This is the spot in our (nRow x nColumn x nSamples) grid array (tensor) where each sample should go.
+switch numel(size(V))
+    case 2 % nChannels x nSamples
+        G = V(idx);
+    case 3 % nChannels x nSamples x nTrials
+        idx = repmat(idx, 1, 1, 1, size(V, 3)) + ...
+            reshape(0:(64 * size(V, 2)):((size(V,3)-1)*64*size(V,2)), 1, 1, 1, size(V, 3));
+        G = V(idx);
+    otherwise
+        error('Unexpected number of dimensions on input array (%d)', numel(size(V)));
+end
 
 end
